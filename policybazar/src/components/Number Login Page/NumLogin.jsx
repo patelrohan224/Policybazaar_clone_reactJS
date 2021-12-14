27799bd66d613b0bd8617a004a4dcad21983790e
Patel Rohan <60181322+patelrohan224@users.noreply.github.com>
2021-12-14 13:03:36 +0530

    main

--- policybazar/src/components/Number Login Page/NumLogin.jsx
+++ policybazar/src/components/Number Login Page/NumLogin.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 export default function NumLogin(){
     return(
         <div className="container-r">
-            <h1>
-                
-            </h1>
+            
         </div>
     )
 }
