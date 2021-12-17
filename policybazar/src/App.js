2a6b1dc9f06a21066387632befecb3c7d39101b2
Patel Rohan <60181322+patelrohan224@users.noreply.github.com>
2021-12-17 23:14:51 +0530

    main

--- policybazar/src/App.js
+++ policybazar/src/App.js
@@ -1,4 +1,5 @@
 import { Route, Switch } from 'react-router-dom';
+import Login from './components/Login page/Logni';
 import Numbike from './components/Number BikePage/Numbike';
 import PolicySteps from './components/Policy detail page/PolicySteps';
 import Twheelmake from "./components/vehicle details/Twheelmake"
@@ -6,7 +7,7 @@ import Bikenotodetails from './Routes/Bike no to Bike details/BikeNotoDetails';
 function App() {
   return (
     <>
-    <Bikenotodetails />
+    {/* <Bikenotodetails /> */}
     <Switch>
     <Route path="/bikeno" exact>
     <Numbike />
@@ -15,7 +16,8 @@ function App() {
    <Twheelmake />
    </Route>
   </Switch>
-  <PolicySteps />
+  {/* <PolicySteps /> */}
+  <Login/>
   </>
   );
 }
